abc1dbaa15510f6b67faa23f3c3866f46835baa9
Shac Ron <sron@nvidia.com>
2022-11-26 16:20:15 -0800

    Fix linux build

--- tools.mk
+++ tools.mk
@@ -14,12 +14,19 @@ LLVM_SOURCE_DIR := $(BLD_HOST_OBJDIR)/$(LLVM_SOURCE)
 LLVM_BUILD_DIR := $(BLD_HOST_OBJDIR)/llvm-build
 # LLVM_RUNTIME_DIR := $(ROOTDIR)/runtime
 
+ifeq ($(BLD_HOST_LD),ld.lld)
+# Clang does not like '-fuse-ld=ld.lld'
+LLVM_LD := lld
+else
+LLVM_LD := $(BLD_HOST_LD)
+endif
+
 BUILDOPTS := \
 	-DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;lld" \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DCMAKE_C_COMPILER=$(BLD_HOST_CC) \
 	-DCMAKE_CXX_COMPILER=$(BLD_HOST_CXX) \
-	-DLLVM_USE_LINKER=$(BLD_HOST_LD) \
+	-DLLVM_USE_LINKER=$(LLVM_LD) \
 	-DCMAKE_INSTALL_PREFIX=$(BLD_TOOLCHAIN_HOST_ROOT)
 
 all: toolchain
@@ -27,18 +34,21 @@ all: toolchain
 # download
 $(LLVM_ARCHIVE_PATH):
 	@mkdir -p $(dir $@)
-	wget $(LLVM_URL) -P $(dir $@)
+	@echo "[curl] " $@
+	@curl --create-dirs -L --output $(LLVM_ARCHIVE_PATH) $(LLVM_URL)
 
 # extract
 $(LLVM_SOURCE_DIR): $(LLVM_ARCHIVE_PATH)
 	@mkdir -p $(dir $@)
 	tar -xzf $(LLVM_ARCHIVE_PATH) -C $(dir $@)
+	@echo "[tar]" $^
+	@tar -xf $(LLVM_ARCHIVE_PATH) -C $(dir $@)
 
 # build
 toolchain: $(LLVM_SOURCE_DIR)
 	@rm -rf $(LLVM_BUILD_DIR) $(BLD_RUNTIME_ROOT)/builtins
 	@mkdir -p $(LLVM_BUILD_DIR) $(BLD_RUNTIME_ROOT)
 	@cp -r $(LLVM_SOURCE_DIR)/compiler-rt/lib/builtins $(BLD_RUNTIME_ROOT)
-	cd $(LLVM_BUILD_DIR) && cmake -G "Unix Makefiles" "$(LLVM_SOURCE_DIR)/llvm" $(BUILDOPTS)
-	$(MAKE) -C $(LLVM_BUILD_DIR)
-	$(MAKE) -C $(LLVM_BUILD_DIR) install
+	@cd $(LLVM_BUILD_DIR) && cmake -G "Unix Makefiles" "$(LLVM_SOURCE_DIR)/llvm" $(BUILDOPTS)
+	@$(MAKE) -C $(LLVM_BUILD_DIR)
+	@$(MAKE) -C $(LLVM_BUILD_DIR) install
