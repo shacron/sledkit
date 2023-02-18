ec7d2715d0d71bcbbd81f17510fab3b7caf96dd0
Shac Ron <shac.ron@gmail.com>
2023-02-17 17:00:38 -0800

    Use LLVM 15.0.6

--- config.mk
+++ config.mk
@@ -21,7 +21,7 @@ BLD_TARGET_IMAGE_BASE ?= 0x10000
 
 # version of llvm and clang to use
 # leave blank if using a different toolchain
-BLD_LLVM_VERSION ?= 15.0.4
+BLD_LLVM_VERSION ?= 15.0.6
 
 MAKE ?= make
 
