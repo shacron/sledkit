611db3ae4d6634dc75461fb4b14cc51f22ad4ecf
Shac Ron <shac.ron@gmail.com>
2023-02-19 21:35:54 -0800

    psled - with objects

--- bindings/python/bind.c
+++ bindings/python/bind.c
@@ -6,33 +6,78 @@
 #include <sled/error.h>
 #include <sled/machine.h>
 
-static void pysled_machine_destroy(PyObject *o) {
-    machine_destroy(PyCapsule_GetPointer(o, "machine_t"));
-}
+typedef struct {
+    PyObject_HEAD
+    machine_t *m;
+} MachineObject;
 
-static PyObject * pysled_machine_create(PyObject *self, PyObject *args) {
-    int err = 0;
-    machine_t *m = NULL;
-    if ((err = machine_create(&m))) {
+static PyObject *psled_machine_new(PyTypeObject *type, PyObject *args, PyObject *kwds) {
+    MachineObject *self;
+    self = (MachineObject *)type->tp_alloc(type, 0);
+    if (self == NULL) return NULL;
+
+    int err = machine_create(&self->m);
+    if (err) {
         PyErr_SetString(PyExc_TypeError, st_err(err));
+        Py_TYPE(self)->tp_free((PyObject *)self);
         return NULL;
     }
-    return PyCapsule_New(m, "machine_t", pysled_machine_destroy);
+    return (PyObject *)self;
 }
 
-static PyMethodDef pysled_methods[] = {
-    { "machine_create",  pysled_machine_create, METH_VARARGS, "Create a machine." },
-    { NULL, NULL, 0, NULL }        // end
+static void psled_machine_dealloc(MachineObject *self) {
+    machine_destroy(self->m);
+    Py_TYPE(self)->tp_free((PyObject *)self);
+}
+
+static PyTypeObject MachineType = {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    .tp_name = "psled.machine",
+    .tp_doc = PyDoc_STR("Sled Machine"),
+    .tp_basicsize = sizeof(MachineObject),
+    .tp_itemsize = 0,
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_new = psled_machine_new,
+    .tp_dealloc = (destructor)psled_machine_dealloc,
 };
 
-static struct PyModuleDef pysledmodule = {
+// static PyObject * psled_machine_create(PyObject *self, PyObject *args) {
+//     // int err = 0;
+//     // machine_t *m = NULL;
+//     // if ((err = machine_create(&m))) {
+//     //     PyErr_SetString(PyExc_TypeError, st_err(err));
+//     //     return NULL;
+//     // }
+//     // return PyCapsule_New(m, "machine_t", psled_machine_destroy);
+// }
+
+// static PyMethodDef psled_methods[] = {
+//     { "machine_create",  psled_machine_create, METH_VARARGS, "Create a machine." },
+//     { NULL, NULL, 0, NULL }        // end
+// };
+
+static struct PyModuleDef psledmodule = {
     PyModuleDef_HEAD_INIT,
-    "sled", // name of module
-    NULL,   // module documentation, may be NULL
-    -1,     // size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
-    pysled_methods
+    .m_name = "sled", // name of module
+    .m_doc = NULL,   // module documentation, may be NULL
+    .m_size = -1,     // size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
+    // .m_methods = psled_methods,
 };
 
-PyMODINIT_FUNC PyInit_pysled(void) {
-    return PyModule_Create(&pysledmodule);
+PyMODINIT_FUNC PyInit_psled(void) {
+    PyObject *m;
+    if (PyType_Ready(&MachineType) < 0)
+        return NULL;
+
+    m = PyModule_Create(&psledmodule);
+    if (m == NULL) return NULL;
+
+    Py_INCREF(&MachineType);
+    if (PyModule_AddObject(m, "machine", (PyObject *) &MachineType) < 0) {
+        Py_DECREF(&MachineType);
+        Py_DECREF(m);
+        return NULL;
+    }
+
+    return m;
 }
--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup, Extension
 
 def main():
   setup(
-    name="pysled",
+    name="psled",
     version="1.0.0",
     description="Sled emulator extension in python",
     author="sledKit project",
     author_email="",
-    ext_modules=[Extension("pysled", ["bindings/python/bind.c"])]
+    ext_modules=[Extension("psled", ["bindings/python/bind.c"])]
   )
 
 if (__name__ == "__main__"):
