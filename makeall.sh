3eb9c2088f5cee3d875c2996a1d8b6c4c5474e04
Shac Ron <shac.ron@gmail.com>
2023-05-23 17:46:28 -0700

    build float-enabled targets

--- makeall.sh
+++ makeall.sh
@@ -6,7 +6,10 @@ if [[ $OSTYPE != 'darwin'* ]]; then
     BUILDOPTS="$BUILDOPTS -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DLLVM_USE_LINKER=/usr/bin/ld.lld"
 fi
 
-arches=(rv32i rv32im rv32ima rv32ic rv32imc rv32imac rv64i rv64im rv64ima rv64ic rv64imc rv64imac)
+# The canonical order is "IMAFDQLCBKJTPVH".
+
+arches=(rv32i rv32im rv32imaf rv32ic rv32imc rv32imafc
+        rv64i rv64im rv64imaf rv64ic rv64imc rv64imafc)
 
 for a in ${arches[@]}; do
     make -j $jobs BLD_TARGET_ARCH=$a
