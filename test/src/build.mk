9b7f2a0c0ba5782bb5c97d6001ff4d183b4e46bc
Shac Ron <shac.ron@gmail.com>
2024-10-17 20:54:54 -0700

    Simple crash generator

--- test/src/build.mk
+++ test/src/build.mk
@@ -1,5 +1,6 @@
 
 atomic_CSOURCES := src/common/atomic.c
+crash_CSOURCES := src/common/crash.c
 fib_CSOURCES := src/common/fib.c
 float_CSOURCES := src/common/float.c
 memset_CSOURCES := src/common/memset.c
